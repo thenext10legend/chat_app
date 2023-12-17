6afb0a1ced1975ba8b43f25433c243a7c81f3bd7
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-12-17 20:10:10 +0530

    Adding Buttons & Modes to the Authentication Screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -8,6 +10,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +48,28 @@ class _AuthScreenState extends State<AuthScreen> {
                               labelText: "Password",
                             ),
                             obscureText: true,
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                            child: Text(_isLogin ? "LogIn" : "SignUp"),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? "Create an account"
+                                : "I already have an account"),
                           )
                         ],
                       ),
