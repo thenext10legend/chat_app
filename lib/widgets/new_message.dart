262379a9e12085b4c19d4784064464bb1098439d
Shivam JhunJhunwala <93035500+thenext10legend@users.noreply.github.com>
2024-02-21 00:35:16 +0530

    290. Sending & Reading Data To & From Firestore

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -8,7 +10,7 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
-  var _messageController = TextEditingController();
+  final _messageController = TextEditingController();
 
   @override
   void dispose() {
@@ -17,14 +19,33 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
+    FocusScope.of(context).unfocus();
     _messageController.clear();
+
+    //getting current user
+    final user = FirebaseAuth.instance.currentUser!;
+
+    //retreive data from user collections
+    final userData = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user.uid)
+        .get();
+
+    //send to firebase
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
   }
 
   @override
